169e0d6ef2e74fcef98e3925f3803045919cd576
Ivan Mitskovets <ivan.mitskovets@gmail.com>
2015-10-15 21:53:36 +0300

    in trouble place choicer add on back pressed

--- app/src/main/java/ivan/mitskovets/dormitory/system/dgaphub/TroublePlaceChoicerActivity.java
+++ app/src/main/java/ivan/mitskovets/dormitory/system/dgaphub/TroublePlaceChoicerActivity.java
@@ -2,10 +2,17 @@ package ivan.mitskovets.dormitory.system.dgaphub;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class TroublePlaceChoicerActivity extends AppCompatActivity {
 
@@ -13,14 +20,23 @@ public class TroublePlaceChoicerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trouble_place_choicer);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_tro_pla_cho);
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_24dp);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("CatalogClient", "button navigation");
+                onBackPressed();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Upload you own photo of trouble", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
