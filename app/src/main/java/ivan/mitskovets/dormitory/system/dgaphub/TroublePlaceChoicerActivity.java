bdce47a01b2edfe4fa9478e09852e76e61d21f61
Ivan Mitskovets <ivan.mitskovets@gmail.com>
2015-10-15 15:56:12 +0300

    create trouble place choicer teme

---
+++ app/src/main/java/ivan/mitskovets/dormitory/system/dgaphub/TroublePlaceChoicerActivity.java
@@ -0,0 +1,13 @@
+package ivan.mitskovets.dormitory.system.dgaphub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TroublePlaceChoicerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trouble_place_choicer);
+    }
+}
