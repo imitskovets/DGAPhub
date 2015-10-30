b67f7c525428072fc8b612fe70c9eb39c1b3371d
Ivan Mitskovets <ivan.mitskovets@gmail.com>
2015-10-31 01:11:19 +0300

    start do report class(update)

--- app/src/main/java/ivan/mitskovets/dormitory/system/dgaphub/Report.java
+++ app/src/main/java/ivan/mitskovets/dormitory/system/dgaphub/Report.java
@@ -1,8 +1,16 @@
 package ivan.mitskovets.dormitory.system.dgaphub;
 
+import android.os.AsyncTask;
+
 
 public class Report {
     int reportCodeInt = 0;
     String reportCodeStr = "";
+
+    String send (){
+        String responseCode = "0";
+        // call reportSender;
+        return responseCode;
+    }
 
 }
---
+++ app/src/main/java/ivan/mitskovets/dormitory/system/dgaphub/ReportSender.java
@@ -0,0 +1,71 @@
+package ivan.mitskovets.dormitory.system.dgaphub;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by ivanm on 9/24/2015.
+ */
+public class ReportSender extends AsyncTask<String, Void, String> {
+
+    public String requestBody="";
+    protected void onPreExecute(){
+        super.onPreExecute();
+    }
+    @Override
+    protected String doInBackground(String... urls) {
+        String output = null;
+        for (String url : urls) {
+            output = getOutputFromUrl(url);
+        }
+        return output;
+    }
+
+    private String getOutputFromUrl(String url) {
+        String responseCode = "empty_form_Serv_Con_getOutputFromUrl";
+        try {
+            responseCode = getHttpConnection(url);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return responseCode;
+    }
+
+    private String getHttpConnection(String urlString) throws IOException {
+        String responseCode = "default_responseCode(something seams wrong)";
+        try {
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+            HttpURLConnection httpConnection = (HttpURLConnection) connection;
+            httpConnection.setRequestMethod("POST");
+            httpConnection.setRequestProperty("User-Agent", "Mozilla/5.0 ( compatible ) ");
+            httpConnection.setRequestProperty("Accept", "*/*");
+            httpConnection.setDoOutput(true);
+            httpConnection.connect();
+            OutputStreamWriter writer = new OutputStreamWriter(httpConnection.getOutputStream());
+            String urlParameters = requestBody;
+            writer.write(urlParameters);
+            writer.flush();
+            writer.close();
+            responseCode = String.valueOf(httpConnection.getResponseCode());
+            Log.v("CatalogClient", "Response code:" + responseCode);
+            httpConnection.disconnect();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+        return responseCode;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        //AsyncTask END
+    }
+}
